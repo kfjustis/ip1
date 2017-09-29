9dfc5fa6a642327a7d2eb62e983dd059a2c2111a
kfjustis <kfjustis@gmail.com>
2017-09-28 20:44:35 -0500

    Loads source image successfully

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,19 @@
 
 int main(int argc, char** argv) {
 
-	/*if (argc != 2) {
+	if (argc != 2) {
 		std::cout << "Invalid command line args\nUSAGE: " << argv[0] << " <image file>" << std::endl;
 		return -1;
-	}*/
+	}
 
 	// load image
 	cv::Mat img;
-	//img = cv::imread(argv[1],1);
+	// 0 flag for grayscale image
+	img = cv::imread(argv[1],0);
 
 	// show image
-	//cv::namedWindow("Greyon", CV_WINDOW_AUTOSIZE);
-	//cv::imshow("Greyon", img);
-	std::cout << "We linked opencv2 and built. Waiting..." << std::endl;
+	cv::namedWindow("Source image", CV_WINDOW_NORMAL);
+	cv::imshow("Source image", img);
 	cv::waitKey(0);
 
 	return 0;
